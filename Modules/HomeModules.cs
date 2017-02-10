8991e33d0e992f66a353c1f068c2de04d3065e83
Nicole Sanders <nsanders9022@gmail.com>
2017-02-10 08:57:36 -0800

    add get and post methods to the HomeModules.cs page

---
+++ Modules/HomeModules.cs
@@ -0,0 +1,32 @@
+using Nancy;
+using AddressBook.Objects;
+using System.Collections.Generic;
+
+namespace AddressBook
+{
+  public class HomeModule : NancyModule
+  {
+    public HomeModule()
+    {
+      Get["/"] = _ => {
+        Contact allContacts = Contact.GetAll();
+        return View["index.cshtml", allContacts];
+      };
+
+      Get["/contact/add"] = _ => {
+        return View["contact_form.cshtml"];
+      };
+
+      Get["/contact/new/{id}"] = parameters => {
+        Contact contact = Contact.Find(parameters.id);
+        return View["contact.cshtml"];
+      };
+
+      Post["/"] = _ => {
+        Contact newContact = new Contact(Request.Form["new-name"], Request.Form["new-phone-number"], Request.Form["new-address"]);
+        List<Contact> allContacts = Contact.GetAll()
+        return View["index.cshtml", allContacts];
+      };
+    }
+  }
+}
