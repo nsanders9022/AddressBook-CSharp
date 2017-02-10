d335d4427c5d69be3526b344243bc084fc7afe07
Nicole Sanders <nsanders9022@gmail.com>
2017-02-10 08:31:24 -0800

    add the contact_form.cshtml page

---
+++ Objects/Contact.cs
@@ -0,0 +1,66 @@
+using System.Collections.Generic;
+
+namespace AddressBook.Objects
+{
+  public class Contact
+  {
+    private static List<Contact> _instances = new List<Contact>{};
+    private string _name;
+    private string _phoneNumber;
+    private string _address;
+
+    public Contact(string contactName, string contactPhoneNumber, string contactAddress)
+    {
+      _name = contactName;
+      _phoneNumber = contactPhoneNumber;
+      _address - contactAddress;
+      _instances.Add(this);
+      _id = _instances.Count;
+    }
+
+    public string GetName()
+    {
+      return _name;
+    }
+
+    public void Setname(string aName)
+    {
+      _name = aName;
+    }
+
+    public string GetPhoneNumber()
+    {
+      return _phoneNumber;
+    }
+
+    public void SetPhoneNumber(string aPhoneNumber)
+    {
+      _phoneNumber = aPhoneNumber;
+    }
+
+    public string GetAddress()
+    {
+      return _address;
+    }
+
+    public void SetAddress(string anAddress)
+    {
+      _address =  anAddress;
+    }
+
+    public static List<Contact> GetAll()
+    {
+      return _instances;
+    }
+
+    public static void Clear()
+    {
+      _instances.Clear();
+    }
+
+    public static Category Find(int searchId)
+    {
+      return _instances[searchId-1];
+    }
+  }
+}
